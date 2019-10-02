e451ca23f7b28a277119866d820906ab2fed4aae
Godzil <godzil@godzil.net>
2019-10-02 19:09:51 +0100

    Update sample vblank code

--- samplesplash_vblank.s
+++ samplesplash_vblank.s
@@ -1,2 +1,10 @@
+
 vblank:
-    ret
+    pusha
+    add word [SS:0xFE00], 0x111
+    mov AL, byte [SS:0x7002]
+    out 0x15, AL
+    inc byte [SS:0x7001]
+    inc byte [SS:0x7002]
+    POPA
+    RETF
